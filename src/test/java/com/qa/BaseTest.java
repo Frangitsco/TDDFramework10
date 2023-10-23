8b52951101bcfa49860ba4e7b17721aba1594628
Francesco Ponzo <mr.franceso.ponzo@gmail.com>
2023-10-23 16:03:57 +0100

    Automated appium start succesfully implement

--- src/test/java/com/qa/BaseTest.java
+++ src/test/java/com/qa/BaseTest.java
@@ -7,13 +7,18 @@ import com.qa.utils.TestUtils;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import io.appium.java_client.service.local.flags.GeneralServerFlag;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Properties;
 
 import org.openqa.selenium.WebElement;
@@ -21,17 +26,55 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 	protected static AppiumDriver driver;
 	protected static Properties props;
+	private static AppiumDriverLocalService server;
 	InputStream inputStream;
 	
 	public BaseTest() {
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 	
+
+
+	@BeforeSuite
+	public void beforeSuite() {
+        server = getAppiumService();
+		server.start();
+		System.out.println("Appium server started");
+	}
+	
+	
+	@AfterSuite
+	public void afterSuit() {
+		server.stop();
+		System.out.println("Appium server stopped");
+	}
+	
+	//public AppiumDriverLocalService getAppiumServerDefault() {
+	//	return AppiumDriverLocalService.buildDefaultService();
+	//}
+	
+	public AppiumDriverLocalService getAppiumService() {
+		HashMap<String, String> environment = new HashMap<String, String>();
+		environment.put("PATH", "/usr/local/Cellar/openjdk/20.0.2/libexec/openjdk.jdk/Contents/Home/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:" + System.getenv("PATH"));
+		environment.put("ANDROID_HOME", "/Users/cex/Library/Android/sdk");
+		return AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+				.usingDriverExecutable(new File("/usr/local/bin/node"))
+				.withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+				.usingPort(4723)
+				.withArgument(GeneralServerFlag.SESSION_OVERRIDE)
+//				.withArgument(() -> "--allow-insecure","chromedriver_autodownload")
+				.withEnvironment(environment));
+//			    .withLogFile(new File("ServerLogs/server.log")));
+	
+	}
+
   @Parameters({"platformName","platformVersion","deviceName"})
   @BeforeTest
   public void beforeTest(String platformName, String platformVersion, String deviceName) throws Exception {
